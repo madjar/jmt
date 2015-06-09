7a53297763affe253c86be989db6527cdc7eb9a5
Georges Dubus <georges.dubus@compiletoi.net>
2015-06-09 14:00:45 +0200

    Support basic script files of the form "character: text".

--- Main.hs
+++ Main.hs
@@ -7,6 +7,7 @@ import Data.Maybe (fromMaybe)
 import Control.Applicative
 import Data.Text (Text)
 import System.Environment
+import Data.Foldable
 
 import Reply
 
@@ -21,10 +22,12 @@ main = do
   apiToken <- fromMaybe (error "SLACK_API_TOKEN not set")
                <$> lookupEnv "SLACK_API_TOKEN"
   args <- getArgs
-  lines <- concat <$> mapM getLines args
-  let replyFunction query = getReply query lines
-  runBot (myConfig apiToken) (textBot replyFunction) ()
+  scripts <- mapM getLines args
+  let bot = textBot $ reply scripts
+  runBot (myConfig apiToken) bot ()
 
+reply :: [Script] -> Text -> Maybe Text
+reply scripts query = asum . map (getReply query) $ scripts
 
 textBot :: (Text -> Maybe Text) -> SlackBot ()
 textBot f (Message cid _ msg _ _ _)
--- Reply.hs
+++ Reply.hs
@@ -2,30 +2,41 @@
 module Reply where
 
 import Text.Subtitles.SRT
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text (parseOnly)
 import qualified Data.ByteString as B
 import qualified Data.Text as T
+import Data.Text (Text)
 import qualified Data.Text.IO as T
 import Data.Text.Encoding
 import Data.Either.Combinators
 import Text.EditDistance
 import Data.Maybe
 
-rawLines :: FilePath -> IO [T.Text]
-rawLines file = map dialog . fromRight' .  parseOnly parseSRT . decodeLatin1 <$> B.readFile file
+type Script = [Text]
 
-cleanLines :: [Text] -> [Text]
-cleanLines = map cleanup . filter notGarbage
+-- Try to read the text as a srt file, otherwise consider it as a script
+readScript :: Text -> Script
+readScript t = fromRight (parseScript t) (readSRT t)
+  where readSRT = fmap (map dialog) . parseOnly parseSRT
+        parseScript = filter (not . T.null) . map (T.strip . T.intercalate ":" . drop 1 . T.splitOn ":") . T.lines
+
+readFileWithUnknownEncoding :: FilePath -> IO Text
+readFileWithUnknownEncoding f = do bytes <- B.readFile f
+                                   return $ fromRight (decodeLatin1 bytes) (decodeUtf8' bytes)
+
+cleanScript :: Script -> Script
+cleanScript = map cleanup . filter notGarbage
   where notGarbage l = not ("--" `T.isPrefixOf` l || "<i>" `T.isPrefixOf` l)
         cleanup = T.replace "\n" " "
 
-getLines s = cleanLines <$> rawLines s
+getLines :: FilePath -> IO Script
+getLines s = cleanScript . readScript <$> readFileWithUnknownEncoding s
 
-
+searchLine :: Text -> Script -> [Text]
 searchLine query = filter (query `T.isInfixOf`)
 
 -- |Search a line among a list of line and returns the list of matches, and their replies
-searchReply :: Text -> [Text] -> [(Text, Text)]
+searchReply :: Text -> Script -> [(Text, Text)]
 searchReply query lines = filter ((< 5) . distance query . fst) (zip lines (tail lines))
   where distance t1 t2 = levenshteinDistance defaultEditCosts (T.unpack t1) (T.unpack t2)
 
